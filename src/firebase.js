4b9367c952c6b88b277dfc551df5fca4a016a53b
Aurelian <aurelians@libero.it>
2020-03-23 23:44:39 +0000

    Updated firebase config file with hardcoded info

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import "firebase/database";
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyBnCTS77x1023nAXYEPY7nk5fkLoqHXEtA",
+    authDomain: "melbourne-tours-coding-test.firebaseapp.com",
+    databaseURL: "https://melbourne-tours-coding-test.firebaseio.com",
+    projectId: "melbourne-tours-coding-test",
+    storageBucket: "melbourne-tours-coding-test.appspot.com",
+    messagingSenderId: "661859946727",
+    appId: "1:661859946727:web:3768aa4ab67fbf43f59242"
 };
 
 firebase.initializeApp(firebaseConfig)
