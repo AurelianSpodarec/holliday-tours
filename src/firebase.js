137adddc591cdadca8934cf05ed2fa508e8501cb
Aurelian <aurelians@libero.it>
2020-03-23 23:54:30 +0000

    Updated firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import "firebase/database";
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBnCTS77x1023nAXYEPY7nk5fkLoqHXEtA",
-    authDomain: "melbourne-tours-coding-test.firebaseapp.com",
-    databaseURL: "https://melbourne-tours-coding-test.firebaseio.com",
-    projectId: "melbourne-tours-coding-test",
-    storageBucket: "melbourne-tours-coding-test.appspot.com",
-    messagingSenderId: "661859946727",
-    appId: "1:661859946727:web:3768aa4ab67fbf43f59242"
+    apiKey: "AIzaSyCBOJ3bb2cTT_GmmWgGAz5o7mWEGljnwTE",
+    authDomain: "holliday-tours.firebaseapp.com",
+    databaseURL: "https://holliday-tours.firebaseio.com",
+    projectId: "holliday-tours",
+    storageBucket: "holliday-tours.appspot.com",
+    messagingSenderId: "239732502417",
+    appId: "1:239732502417:web:d13fd6d61beba1f4e27430"
 };
 
 firebase.initializeApp(firebaseConfig)
