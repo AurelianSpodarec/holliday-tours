a91678a731937c2f581b32bef35fb3e8a8141cd2
Aurelian <aurelians@libero.it>
2020-03-23 23:10:03 +0000

    Fixed: Theme now linking to correct folder.

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -6,7 +6,7 @@ const useStyles = makeStyles(theme => ({
         borderRadius: '50%',
         flexGrow: 0,
         flexShrink: 0,
-        // padding: theme.spacing(1),
+        padding: theme.spacing(1),
         backgroundColor: 'red'
     }
 }));
